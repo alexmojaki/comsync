5937532f8db820ea148564ee366abda9b03fe740
Alex Hall <alex.mojaki@gmail.com>
2022-03-06 21:44:39 +0200

    Reduce bundle size: no source maps, use peer dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,8 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
-  devtool: 'inline-source-map',
+  externals: {
+    comlink: 'comlink',
+    'sync-message': 'sync-message',
+  }
 };
